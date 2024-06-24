4574e8664312ba2bc8f832cf5c8543bf3d67fc22
Marcos Almeida <marcosviniciosalm@gmail.com>
2024-06-24 20:39:41 -0300

    LAB HARDWARE

---
+++ Algorítmo/desafios/ex112/app.js
@@ -0,0 +1,16 @@
+let btn = document.querySelector('#btn')
+let div = document.querySelector('#res')
+let numInput = document.querySelector('#numero')
+
+function validar() {
+    let numero = parseFloat(numInput.value)
+
+    div.innerHTML = ''
+
+    for (let i = 2; i >= 2; i++) {
+        if (numero)
+    }
+
+}
+
+btn.addEventListener('click', validar)
