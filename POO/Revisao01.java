05a18a593e4ef9907e620fd6bea159465d72676a
Marcos Almeida <80133181+NessKawl@users.noreply.github.com>
2025-02-13 12:34:53 -0300

    Aula 01 POO - 12/02/2025

---
+++ POO/Revisao01.java
@@ -0,0 +1,22 @@
+/*
+	Revisão01.java
+ */
+
+
+public class Revisao01 {
+	
+	public static void main (String[] args) {
+
+		int idade = 50;
+		
+		double peso = 68, altura = 1.78;
+		
+		String nome = "Joseverson";
+		
+		boolean situacao = true;
+		
+		System.out.print(idade);
+		
+	}
+}
+
